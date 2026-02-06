f03181802958a74f0cb0c2ece46a3507e7de8e18
PzD3v <ppesantez97@gmail.com>
2026-02-06 16:32:10 -0500

    UseReducer-Context-Provider ok

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,15 +2,18 @@ import rigoImageUrl from "../assets/img/rigo-baby.jpg";
 import useGlobalReducer from "../hooks/useGlobalReducer.jsx";
 
 export const Home = () => {
+  const getApi = async () => {
+	const response = fetch("https://www.swapi.tech/api/people")
+  }
+  
+  const { store, dispatch } = useGlobalReducer();
 
-  const {store, dispatch} =useGlobalReducer()
-
-	return (
-		<div className="text-center mt-5">
-			<h1>Hello Rigo!!</h1>
-			<p>
-				<img src={rigoImageUrl} />
-			</p>
-		</div>
-	);
-}; 
+  return (
+    <div className="text-center mt-5">
+      <h1>Hello Rigo!!</h1>
+      <p>
+        <img src={rigoImageUrl} />
+      </p>
+    </div>
+  );
+};
---
+++ src/reducers/starWarsReducer.js
@@ -0,0 +1,36 @@
+export const initialState = {
+  people: [],
+  planets: [],
+  vehicles: [],
+  favorites: [], // Aqui guardaremos lo que el usuario eligio
+};
+export default function starWarsReducer(store, action = {}) {
+  switch (action.type) {
+    case "ADD_FAVORITE":
+      //Buscamos si ya está en la lista
+      const isFavorite = store.favorites.find(
+        (fav) => fav.id === action.payload.id,
+      );
+
+      if (isFavorite) {
+        //Si ya está, lo eliminamos para que no se repita
+        return {
+          ...store,
+          favorites: store.favorites.filter(
+            (fav) => fav.id !== action.payload.id,
+          ),
+          //El método .filter() crea una lista nueva SIN el elemento que ya estaba.
+        };
+      } else {
+        //Si no está, lo agregamos
+
+        return {
+          ...store,
+          favorites: [...store.favorites, action.payload],
+        };
+      }
+
+    default:
+      return store;
+  }
+}
